356434c684c1b1e0492cf36a3c048a34577cbc81
David Tang <david.tang@verizondigitalmedia.com>
2019-08-31 00:38:57 -0700

    convert App component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MemberImage from './MemberImage';
 import GitHubRepoCard from './GitHubRepoCard';
 import Loading from './Loading';
 import { fetchMembers, fetchRepos } from './GitHubApi';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      members: [],
-      repos: [],
-      loading: true
-    };
-  }
-  async componentDidMount() {
-    let [ members, repos ] = await Promise.all([
+export default function App() {
+  const [ members, setMembers ] = useState([]);
+  const [ repos, setRepos ] = useState([]);
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    Promise.all([
       fetchMembers(),
       fetchRepos()
-    ]);
-    // let members = await fetchMembers();
-    // let repos = await fetchRepos();
-    this.setState({ members, repos, loading: false });
-  }
-  render() {
-    return (
-      <div>
-        {this.state.loading ? <Loading /> : (
-          <>
-            <div className="members">
-              {this.state.members.map((member) => {
-                return <MemberImage member={member} key={member.id} />
-              })}
-            </div>
-            <div className="repos">
-              {this.state.repos.map((repo) => {
-                return <GitHubRepoCard repo={repo} key={repo.id} />
-              })}
-            </div>
-          </>
-        )}
-      </div>
-    );
-  }
-}
+    ]).then(([ members, repos ]) => {
+      setLoading(false);
+      setMembers(members);
+      setRepos(repos);
+    });
+  }, []);
 
-export default App;
+  return (
+    <div>
+      {loading ? <Loading /> : (
+        <>
+          <div className="members">
+            {members.map((member) => {
+              return <MemberImage member={member} key={member.id} />
+            })}
+          </div>
+          <div className="repos">
+            {repos.map((repo) => {
+              return <GitHubRepoCard repo={repo} key={repo.id} />
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
